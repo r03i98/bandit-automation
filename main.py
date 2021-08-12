a0a61cba669c8ca2c246e48871eae19db7de487a
roeihadashi <roeihadashi@gmail.com>
2021-08-12 20:06:19 +0300

    added user args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import argparse
 
 class AutoBandit:
     def __init__(self):
@@ -8,10 +9,16 @@ class AutoBandit:
     def bandit_command(self):
         os.system('bandit --quiet -f json -r vuln_apps/ -o results.json')
 
+    def manual_pt(self):
+
     def json_manipulte(self):
         f = open('results.json')
         data = json.load(f)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pt', type=int, help='Listen port. Default: 4000', default=4000)
+    args = parser.parse_args()
 
-myclass = AutoBandit()
-myclass.bandit_command()
+    myclass = AutoBandit()
+    myclass.bandit_command()
